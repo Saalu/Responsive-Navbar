d47d8a217108122054d01ce03853e3d358c66547
Saalu <saaluissaka@gmail.com>
2023-01-29 22:05:35 +0000

    3 files modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const menuIcon = document.getElementById('menu')
+const menuIcon = document.getElementById('menu');
+const list = document.querySelector('.nav');
 
-
-menuIcon.addEventListener('click', function(){
-  
-})
+menuIcon.addEventListener('click', function () {
+  list.classList.toggle('show');
+});
